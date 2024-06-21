76d0f7bc94e35b610d3d3e8e63c7e31c8cd83c8b
Francisco Ávila <50455756+7oni3c@users.noreply.github.com>
2024-06-20 18:51:01 -0600

    update
    - Devolución de compra
    - Determinar estados

---
+++ src/compras/_static/js/procesos.js
@@ -0,0 +1,56 @@
+var procesar =
+{
+    init()
+    {
+        const form_contado = document.getElementById("form_liquidacion_contado");
+        const form_credito = document.getElementById("form_liquidacion_credito");
+        const form_consignado = document.getElementById("form_liquidacion_consignado");
+        const form_noaplica = document.getElementById("form_liquidacion_na");
+        const form_devolucion = document.getElementById("form_devolucion");
+        
+        if (form_credito) this.credito.init(form_credito);
+        if (form_devolucion) this.devolucion.init(form_devolucion);
+    },
+
+    contado:{},
+    credito:{
+        form:null, fields:null,
+
+        init(form)
+        {
+            this.form = form;
+            this.fields = form.elements;
+            const sel_divisa = this.fields["sel_divisa"];
+            const txt_tcambio = this.fields["txt_tcambio"];
+
+            sel_divisa.addEventListener("change", () => {
+                const opt_divisa = sel_divisa.options[sel_divisa.selectedIndex];
+                txt_tcambio.value = Number(opt_divisa.getAttribute("data-tcambio") ?? "1");
+            });
+        },
+    },
+    consignado:{},
+    noaplica:{},
+    devolucion:{
+        form:null, fields:null,
+
+        init(form)
+        {
+            this.form = form;
+            this.fields = form.elements;
+            const rdb_deposito = this.fields["rdb_deposito"];
+            const rdb_bonifi = this.fields["rdb_bonificacion"];
+            const div_cuenta = document.getElementById("div_sel_cuenta");
+            const div_ingreso = document.getElementById("div_cat_ingreso");
+
+            rdb_deposito.addEventListener("focus", () => {
+                div_cuenta.hidden = false;
+                div_ingreso.hidden = false;
+            });
+            rdb_bonifi.addEventListener("focus", () => {
+                div_cuenta.hidden = true;
+                div_ingreso.hidden = true;
+            });
+        },
+    },
+}
